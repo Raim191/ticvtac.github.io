052f470f7b145d687a98aa991df1f015dfa72423
raim121 <raimraiyan596@gmail.com>
2022-12-26 15:55:08 +0600

    hello

---
+++ hello.js
@@ -0,0 +1,18 @@
+var yo = document.querySelectorAll("button")[0].addEventListener("click",function(){
+
+    var hel = this.innerHTML;
+
+    document.querySelector("h1").innerHTML=hel;
+})
+
+var yo = document.querySelectorAll("button")[1].addEventListener("click",function(){
+
+    document.querySelector("h1").innerHTML="Bye";
+})
+
+var yo = document.querySelectorAll("button")[2].addEventListener("click",function(){
+
+    document.querySelector("h1").innerHTML="yo";
+})
+fh4
+
